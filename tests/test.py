962735028d052cbdd7bcd371cb7b3d631be70c03
thadler <t.hadler@aol.de>
2024-07-14 17:35:45 +0200

    Basic starting repo

---
+++ tests/test.py
@@ -0,0 +1,18 @@
+import os, unittest, logging, glob
+
+class ExampleTest(unittest.TestCase):
+
+    def setUp(self):
+        # set class variables to be used here
+        pass
+
+    def test_functionname1(self):
+        # assert relationships and numerical values here
+        #self.assertAlmostEqual(true_value, calculatated_value, places=3, msg='Failed for ...')
+        #self.assertIn(value, iterable_class_element, msg='Failed for ...')
+        pass
+
+    
+
+if __name__ == '__main__':
+    unittest.main()
